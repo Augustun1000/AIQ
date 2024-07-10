b40e903940e6a3ad4ad476aebe1ba99b46cc0217
Augustun1000 <104395373+Augustun1000@users.noreply.github.com>
2024-07-10 15:47:48 -0300

    Add files via upload

--- Basic-Installer-Netbook.sh
+++ Basic-Installer-Netbook.sh
@@ -6,11 +6,11 @@ chmod 777 Install-Config.sh &&
 chmod 777 Vanities-Installer.sh &&
 chmod 777 Delete-All-Scripts.sh &&
 shopt -s dotglob &&
-rm -rf /home/augustun1000/AIO/.git &&
-mv /home/augustun1000/AIO/* /home/augustun1000/ &&
-rm -rf /home/augustun1000/AIO &&
+rm -rf /home/augustun1000/AIQ/.git &&
+mv /home/augustun1000/AIQ/* /home/augustun1000/ &&
+rm -rf /home/augustun1000/AIQ &&
 # List of packages to install
-sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86-video-intel mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-tools qtile obconf yad arandr feh gvfs htop neofetch adapta-gtk-theme networkmanager cbatticon lm_sensors lxappearance-gtk3 nano geany rofi p7zip gimp kitty xterm flameshot baobab xarchiver ristretto meld mplayer papirus-icon-theme ttf-dejavu ttf-liberation noto-fonts gnome-disk-utility gparted noto-fonts-cjk pulseaudio pavucontrol brightnessctl mtpfs gvfs-mtp gvfs-gphoto2 os-prober networkmanager networkmanager-pptp networkmanager-openvpn nm-connection-editor network-manager-applet ncdu chromium
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xdg-user-dirs xf86-video-intel mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-tools openbox obconf yad arandr feh gvfs htop neofetch adapta-gtk-theme tint2 networkmanager cbatticon lm_sensors lxappearance-gtk3 nano geany rofi p7zip gimp kitty xterm flameshot baobab xarchiver viewnior meld mplayer papirus-icon-theme ttf-dejavu ttf-liberation noto-fonts gnome-disk-utility gparted noto-fonts-cjk pulseaudio pavucontrol brightnessctl mtpfs gvfs-mtp gvfs-gphoto2 os-prober networkmanager networkmanager-pptp networkmanager-openvpn nm-connection-editor network-manager-applet ncdu
 
 #Universal intel drivers (All gen 1 to up)
 #sudo pacman -S xf86-video-intel vulkan-intel lib32-vulkan-intel vulkan-tools mesa lib32-mesa
